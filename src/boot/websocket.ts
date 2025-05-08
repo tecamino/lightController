5b6b56eb865e12fc75633e3eeeb97d1ffde3bcf4
Adrian Zürcher <zuercher@mst.local>
2025-05-08 17:23:16 +0200

    change local ip to dynamic of browser window

--- src/boot/websocket.ts
+++ src/boot/websocket.ts
@@ -4,8 +4,12 @@ import { initWebSocket } from 'src/services/websocket';
 
 export default boot(({ app }) => {
   const $q = app.config.globalProperties.$q as QVueGlobals;
+  const host = window.location.hostname; // gets current domain or IP
+  const port = 8100; // your WebSocket port
 
-  const ws = initWebSocket('ws://127.0.0.1:8100/ws?id=quasar', $q);
+  const randomId = Math.floor(Math.random() * 10001); // random number from 0 to 10000
+  const ws = initWebSocket(`ws://${host}:${port}/ws?id=q${randomId}`, $q);
+
   app.config.globalProperties.$socket = ws;
   ws.connect();
 });
