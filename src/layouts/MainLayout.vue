c58dbf34d0faf49f36e3910ca3de69a0c5976e1c
Adrian Zuercher <adrianzuercher@MacBook-Air-von-Adrian.local>
2025-06-29 20:27:18 +0200

    add icon as asset, add refresh button

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,15 +3,17 @@
     <q-header elevated>
       <q-toolbar>
         <q-img
-          src="src/assets/LOGO_CF-ICON_color.svg"
+          :src="logo"
           alt="Logo"
           style="width: 40px; height: 40px; background-color: var(--q-primary)"
           class="q-mr-sm"
         />
         <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
+
         <q-toolbar-title> Light Control </q-toolbar-title>
 
         <div>Version {{ version }}</div>
+        <q-btn dense icon="refresh" square class="q-px-md q-ml-md" @click="refresh" />
       </q-toolbar>
     </q-header>
 
@@ -35,6 +37,7 @@
 </template>
 
 <script setup lang="ts">
+import logo from 'src/assets/LOGO_CF-ICON_color.svg';
 import { ref } from 'vue';
 import { version } from '../..//package.json';
 
@@ -46,5 +49,9 @@ function toggleLeftDrawer() {
 
 function closeDrawer() {
   leftDrawerOpen.value = false;
+}
+
+function refresh() {
+  window.location.reload();
 }
 </script>
